596a8ce679ca0a60dd6fb506c5a1f81e83836865
halilov-frondend <halilovabdulhafiz2005@gmail.com>
2023-02-16 23:32:03 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// 2 - task
+
+// 1  Ichida "Jazz" va "Blues" qiymatlari bo'lgan array yasang / "Rock-n-roll" ni oxiriga qo'shing
+// const malumot = ['Jazz', 'Blues']
+// malumot.push('Rock-n-roll')
+// console.log(malumot)
+
+// 2 
+// let malumot = ['Jazz', 'Blues', 'Rock-n-roll']
+// let malumot2 =  [1]
+// console.log(malumot)
+
+// 3 Arrayning birinchi qiymatini olib tashlang va console ga chiqaring
+// const malumot = ['Jazz', 'Blues']
+// malumot.shift('Jazz')
+// console.log(malumot)
+
+// 4 "Rap" va "Reggae" qiymatlarni array boshiga qo'shing Natija quyidagicha ko'rinishda chiqsin
+// const malumot = ['Jazz', 'Blues'];
+// malumot.unshift('Rap', "Reggae");
+// const malumot2 = malumot.join(", ");
+// console.log(malumot2)
+
+
+//3 - task
+
+// let salaries = {
+//     "Bob": 1700,
+//     "John": 6000,
+//     "Evelina": 1200
+// }
+
+// let sumSalaries = function (){
+//     let param = 0
+// for (let property in salaries) {
+//     param += salaries[property];
+//     console.log(param)
+// }}
+            
+// console.log(sumSalaries())
+
+
+//4 - task
+
+//     let paaram = [];
+//     let paaram2 = [];
+
+//     function calcSum(paaram) {
+//     let add = 0;
+//     let print = 0;
+//     for (let i = 0; i < paaram.length; i++) {
+//         if (i % 2 == 0)
+//         add += paaram[i];
+//         else
+//         print += paaram[i];
+//     }
+
+//     paaram2.push(print, add);
+//     console.log(paaram2);
+// }
+
+
+// calcSum([6,7,8,9,10]);
